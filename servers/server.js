6cb37808e2011a6fc52cde5790e60a77fd9beb2f
winnyiee@naver.com <winnyiee@naver.com>
2020-05-24 21:42:12 +0900

    fxxx

--- servers/server.js
+++ servers/server.js
@@ -8,8 +8,8 @@ const path = require('path');
 
 // app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
 
+app.use('/', express.static(__dirname + '/'));
 app.use(bodyParser.json());
 app.use('/api', route);
 
